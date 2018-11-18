dddab32d35ecf757137d6462ca2abde142b19655
sf105 <github@m3p.co.uk>
2018-11-18 17:02:18 +0000

    added initial failing test

---
+++ src/tddasifyoumeantit/TicTacToeTests.java
@@ -0,0 +1,15 @@
+package tddasifyoumeantit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+@SuppressWarnings("WeakerAccess")
+public class TicTacToeTests {
+    @Test
+    public void
+    beginHere() {
+        fail("type something");
+    }
+
+}
