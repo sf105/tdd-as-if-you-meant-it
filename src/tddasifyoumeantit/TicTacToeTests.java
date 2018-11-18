dfab821245382dfe0722cf17360599a1cebbc9d2
sf105 <github@m3p.co.uk>
2018-11-18 17:34:32 +0000

    We've introduced a key for the integer values and a new code for X winning. The test now fails

--- src/tddasifyoumeantit/TicTacToeTests.java
+++ src/tddasifyoumeantit/TicTacToeTests.java
@@ -4,6 +4,14 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/*
+   This represents the positions in a game.
+   'X' always starts a game, and players alternate
+      1 | 2 | 3
+      4 | 5 | 6
+      7 | 8 | 9
+ */
+
 @SuppressWarnings("WeakerAccess")
 public class TicTacToeTests {
     @Test public void
@@ -13,6 +21,11 @@ public class TicTacToeTests {
         assertEquals("Unfinished", ticTacToeScoreOf(1, 2));
         assertEquals("Unfinished", ticTacToeScoreOf(1, 2, 3));
         assertEquals("Unfinished", ticTacToeScoreOf(1, 2, 3, 4));
+    }
+
+    @Test public void
+    xWinsWithTheTopRow() {
+        assertEquals("X", ticTacToeScoreOf(1, 4, 2, 5, 3));
     }
 
     private String ticTacToeScoreOf(int... moves) {
